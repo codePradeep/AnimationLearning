8af5c1000bd1f3f5e3110cfe8a44ea48d29f0dfd
codePradeep <pradeep.sharma1@successive.tech>
2022-02-15 14:22:38 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import Animation from './src/Basics';
+
+
+const App = () => {
+
+  return (
+    <Animation />
+   
+  );
+};
+
+
+
+export default App;
---
+++ src/Basics/index.tsx
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   Text,
+   View,
+ } from 'react-native';
+ 
+ 
+ const Animation = () => {
+  
+ 
+ 
+   return (
+     <SafeAreaView >
+       <StatusBar barStyle={'light-content' } />
+       <ScrollView
+         contentInsetAdjustmentBehavior="automatic"
+        
+        >
+         <View>
+          <Text>hello This this new basic app</Text>
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+   );
+ };
+ 
+
+ export default Animation;
+ 
