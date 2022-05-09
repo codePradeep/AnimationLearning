c7a33e9595a87e31d099375efd7793b1b75c05db
codePradeep <pradeep.sharma1@successive.tech>
2022-05-09 12:44:52 +0530

    Firebase cloud

---
+++ src/FirebaseCloudStorage/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+
+export default ()=>{
+    const [name,setName] = useState<number[]|undefined>([])
+    const [hobby,setHobby] = useState('')
+    
+    return(
+    <View>
+        
+        
+        </View>
+        )
+}
+
+
+
