61f60b755901b988593c7f5e5935ec8f8cc422c3
codePradeep <pradeep.sharma1@successive.tech>
2022-07-21 12:28:22 +0530

    refactor

--- src/qrCode/style.ts
+++ src/qrCode/style.ts
@@ -2,7 +2,9 @@ import { StyleSheet } from "react-native";
 
 const styles=StyleSheet.create({
     mainContainer:{
-        flex:1
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
     }
 })
 
